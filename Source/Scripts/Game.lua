87c1fd51208db4ae26c22f68e361e559670aaf65
Rémi Parmentier <remi@hteumeuleu.fr>
2022-12-16 16:40:52 +0100

    fix: tabs

--- Source/Scripts/Game.lua
+++ Source/Scripts/Game.lua
@@ -7,39 +7,39 @@ class("Game").extends()
 
 function Game:init()
 
-    Game.super.init(self)
-    self:addMenuItems()
-    self._init = _init
-    self._update = _update
-    self._draw = _draw
-    self:_init()
-    return self
+	Game.super.init(self)
+	self:addMenuItems()
+	self._init = _init
+	self._update = _update
+	self._draw = _draw
+	self:_init()
+	return self
 
 end
 
 function Game:update()
 
-    self:_update()
-    self:_draw()
+	self:_update()
+	self:_draw()
 
 end
 
 function Game:restart()
 
-    self:_init()
+	self:_init()
 
 end
 
 function Game:addMenuItems()
 
-    local menu = playdate.getSystemMenu()
-    menu:addMenuItem("Restart", function()
-        self:restart()
-    end)
-    menu:addCheckmarkMenuItem("Assist", false, function(value)
-        if value then
-            max_djump=999
-        end
-    end)
+	local menu = playdate.getSystemMenu()
+	menu:addMenuItem("Restart", function()
+		self:restart()
+	end)
+	menu:addCheckmarkMenuItem("Assist", false, function(value)
+		if value then
+			max_djump=999
+		end
+	end)
 
 end
