33f861a6a1caf64545105836dc4011612ea71ced
Rémi Parmentier <remi@hteumeuleu.fr>
2024-01-09 13:49:35 +0100

    feat: update fly_fruit

--- Source/Scripts/Objects/FlyFruit.lua
+++ Source/Scripts/Objects/FlyFruit.lua
@@ -1,11 +1,12 @@
 local pd <const> = playdate
 local gfx <const> = pd.graphics
 local image_table <const> = gfx.imagetable.new("Assets/fruit")
-local img <const> = image_table:getImage(2)
+local img <const> = gfx.image.new(30, 10)
 local sin <const> = pico8.sin
 local max <const> = pico8.max
 local appr <const> = pico8.celeste.appr
 local sign <const> = pico8.celeste.sign
+local flip <const> = pico8.flip
 
 class('FlyFruit').extends(ParentObject)
 
@@ -19,9 +20,9 @@ function FlyFruit:init(x, y, parent)
 	self.sfx_delay = 8
 	self:setImage(img)
 	self:setGroups({4})
-	self:setCollideRect(self.hitbox:offsetBy(1,1))
+	self:setCollideRect(self.hitbox:offsetBy(11,1))
 	self:setCollidesWithGroups({1})
-	self:moveTo(x - 1, y)
+	self:moveTo(x - 11, y)
 	self:setZIndex(20)
 	self:add()
 	return self
@@ -65,7 +66,21 @@ function FlyFruit:_draw()
 	else
 		off = (off + 0.25) % 3
 	end
-	self:moveTo(self.pos.x, self.pos.y)
+
+	local drawFruit=function(img)
+		gfx.pushContext(img)
+			gfx.clear(gfx.kColorClear)
+			local pdtilewing = image_table:getImage(math.floor(3 + off))
+			local pdtilefruit = image_table:getImage(2)
+			pdtilewing:draw(3, -1, flip(true, false))
+			pdtilewing:draw(17, -1)
+			pdtilefruit:draw(10, 0)
+		gfx.popContext()
+	end
+
+	drawFruit(img)
+	self:setImage(img)
+	self:moveTo(self.pos.x - 11, self.pos.y - 1)
 
 end
 
